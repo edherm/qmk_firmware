7ac3ba3795f64e3fc1b417f2b56a1729915b9460
Ed Herman <eah286@cornell.edu>
2025-06-05 14:23:16 -0400

    Add custom keycodes from launcher macros

--- keyboards/keychron/q11/ansi_encoder/keymaps/edherm/keymap.c
+++ keyboards/keychron/q11/ansi_encoder/keymaps/edherm/keymap.c
@@ -22,6 +22,14 @@ enum layers{
     WIN_FN,
 };
 
+enum custom_keycodes {
+    CTL_LEFT = SAFE_RANGE,
+    CTL_RGHT,
+    TMR_NXT,
+    TMR_STRT,
+    MUT_ZM,
+};
+
 enum {
     TD_RTSHFT_CAPS,
     TD_CAPS_CUSTOM,
@@ -59,25 +67,76 @@ void caps_tap_hold_reset(tap_dance_state_t *state, void *user_data) {
 }
 // End cmd+k | ctrl+shift
 
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch(keycode) {
+        case CTL_LEFT:
+            if (record->event.pressed) {
+                register_code(KC_LCTL);
+                tap_code(KC_LEFT);
+                unregister_code(KC_LCTL);
+            }
+            return false;
+        case CTL_RGHT:
+            if (record->event.pressed) {
+                register_code(KC_LCTL);
+                tap_code(KC_RGHT);
+                unregister_code(KC_LCTL);
+            }
+            return false;
+        case TMR_NXT:
+            if (record->event.pressed) {
+                register_code(KC_LCTL);
+                register_code(KC_LOPT);
+                register_code(KC_LCMD);
+                tap_code(KC_S);
+                unregister_code(KC_LCMD);
+                unregister_code(KC_LOPT);
+                unregister_code(KC_LCTL);
+            }
+            return false;
+        case TMR_STRT:
+            if (record->event.pressed) {
+                register_code(KC_LCTL);
+                register_code(KC_LOPT);
+                register_code(KC_LCMD);
+                tap_code(KC_F);
+                unregister_code(KC_LCMD);
+                unregister_code(KC_LOPT);
+                unregister_code(KC_LCTL);
+            }
+            return false;
+        case MUT_ZM:
+            if (record->event.pressed) {
+                register_code(KC_LSFT);
+                register_code(KC_LCMD);
+                tap_code(KC_A);
+                unregister_code(KC_LCMD);
+                unregister_code(KC_LSFT);
+            }
+            return false;
+    }
+    return true;
+}
+
 #define KC_TASK LGUI(KC_TAB)
 #define KC_FLXP LGUI(KC_E)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_91_ansi(
-        KC_MUTE,  KC_ESC,              KC_BRID,  KC_BRIU,  KC_MCTL,     KC_LPAD,  RM_VALD,   RM_VALU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  KC_INS,    KC_DEL,   KC_MUTE,
-        _______,  KC_GRV,              KC_1,     KC_2,     KC_3,        KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,             KC_PGUP,
-        _______,  KC_TAB,              KC_Q,     KC_W,     KC_E,        KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,             KC_PGDN,
-        _______,  TD(TD_CAPS_CUSTOM),  KC_A,     KC_S,     KC_D,        KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,              KC_HOME,
-        _______,  KC_LSFT,             KC_Z,     KC_X,     KC_C,        KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              TD(TD_RTSHFT_CAPS),  KC_UP,
-        _______,  KC_LCTL,             KC_LOPT,  KC_LCMD,  MO(MAC_FN),           KC_SPC,                        KC_SPC,              KC_RCMD,  MO(MAC_FN), KC_RCTL,  KC_LEFT,   KC_DOWN,  KC_RGHT),
+        KC_MUTE,  KC_ESC,              KC_BRID,  KC_BRIU,  KC_MCTL,     KC_LPAD,  RM_VALD,   RM_VALU,  KC_MPRV,   KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  TMR_STRT,  KC_DEL,   KC_MUTE,
+        _______,  KC_GRV,              KC_1,     KC_2,     KC_3,        KC_4,     KC_5,      KC_6,     KC_7,      KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,             KC_PGUP,
+        _______,  KC_TAB,              KC_Q,     KC_W,     KC_E,        KC_R,     KC_T,      KC_Y,     KC_U,      KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,             KC_PGDN,
+        _______,  TD(TD_CAPS_CUSTOM),  KC_A,     KC_S,     KC_D,        KC_F,     KC_G,      KC_H,     KC_J,      KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,              KC_HOME,
+        _______,  KC_LSFT,             KC_Z,     KC_X,     KC_C,        KC_V,     KC_B,      KC_N,     KC_M,      KC_COMM,  KC_DOT,   KC_SLSH,              TD(TD_RTSHFT_CAPS),  KC_UP,
+        MUT_ZM,   KC_LCTL,             KC_LOPT,  KC_LCMD,  MO(MAC_FN),           KC_SPC,                        KC_SPC,              KC_RCMD,  MO(MAC_FN), KC_RCTL,  KC_LEFT,   KC_DOWN,  KC_RGHT),
 
     [MAC_FN] = LAYOUT_91_ansi(
-        RM_TOGG,  _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   _______,                _______,  RM_TOGG,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,                          _______,
-        _______,  RM_TOGG,  RM_NEXT,  RM_VALU,  RM_HUEU,  RM_SATU,  RM_SPDU,   _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,                          _______,
-        _______,  _______,  RM_PREV,  RM_VALD,  RM_HUED,  RM_SATD,  RM_SPDD,   _______,  _______,  _______,  _______,  _______,  _______,              _______,                          _______,
-        _______,  _______,            _______,  _______,  _______,  _______,   _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,                _______,
-        _______,  _______,  _______,  _______,  _______,            _______,                       _______,            _______,  _______,    _______,  _______,                _______,  _______),
+        RM_TOGG,   _______,            KC_F1,    KC_F2,    KC_F3,       KC_F4,     KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   TMR_NXT,                _______,  RM_TOGG,
+        _______,   _______,            _______,  _______,  _______,     _______,   _______,   _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,                          _______,
+        _______,   RM_TOGG,            RM_NEXT,  RM_VALU,  RM_HUEU,     RM_SATU,   RM_SPDU,   _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,                          _______,
+        _______,   _______,            RM_PREV,  RM_VALD,  RM_HUED,     RM_SATD,   RM_SPDD,   _______,  _______,  _______,  _______,  _______,  _______,              _______,                          _______,
+        _______,   _______,            _______,  _______,  _______,     _______,   _______,   NK_TOGG,  _______,  _______,  _______,  _______,              _______,                _______,
+        _______,   _______,            _______,  _______,  _______,               _______,                       _______,            _______,  _______,    _______,  _______,                _______,  _______),
 
     [WIN_BASE] = LAYOUT_91_ansi(
         KC_MUTE,  KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_INS,                 KC_DEL,   KC_MUTE,
@@ -98,8 +157,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [MAC_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [MAC_FN]   = { ENCODER_CCW_CW(RM_VALD, RM_VALU), ENCODER_CCW_CW(RM_VALD, RM_VALU) },
+    [MAC_BASE] = { ENCODER_CCW_CW(CTL_LEFT, CTL_RGHT), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [MAC_FN]   = { ENCODER_CCW_CW(RM_PREV, RM_NEXT), ENCODER_CCW_CW(RM_VALD, RM_VALU) },
     [WIN_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
     [WIN_FN]   = { ENCODER_CCW_CW(RM_VALD, RM_VALU), ENCODER_CCW_CW(RM_VALD, RM_VALU) }
 };
