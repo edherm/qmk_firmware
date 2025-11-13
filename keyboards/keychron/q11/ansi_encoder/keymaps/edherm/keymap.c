340794b802d1f50b25d1c048a5b87d4c4aecaa06
Ed Herman <eah286@cornell.edu>
2025-11-13 09:22:35 -0500

    Open Folder in VSCode

--- keyboards/keychron/q11/ansi_encoder/keymaps/edherm/keymap.c
+++ keyboards/keychron/q11/ansi_encoder/keymaps/edherm/keymap.c
@@ -23,7 +23,8 @@ enum layers{
 };
 
 enum custom_keycodes {
-    CTL_LEFT = SAFE_RANGE,
+    CODE_DIR = SAFE_RANGE,
+    CTL_LEFT,
     CTL_RGHT,
     FLOW_SHRTCT,
     TMR_NXT,
@@ -70,6 +71,15 @@ void caps_tap_hold_reset(tap_dance_state_t *state, void *user_data) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch(keycode) {
+        case CODE_DIR:
+            if (record->event.pressed) {
+                register_code(KC_LSFT);
+                register_code(KC_LCTL);
+                tap_code(KC_V);
+                unregister_code(KC_LCTL);
+                unregister_code(KC_LSFT);
+            }
+            return false;
         case CTL_LEFT:
             if (record->event.pressed) {
                 register_code(KC_LCTL);
@@ -138,7 +148,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_91_ansi(
         KC_MUTE,  KC_ESC,              KC_BRID,  KC_BRIU,  KC_MCTL,     KC_LPAD,  RM_VALD,   RM_VALU,  KC_MPRV,   KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  FLOW_SHRTCT,  KC_DEL,   KC_MUTE,
-        _______,  KC_GRV,              KC_1,     KC_2,     KC_3,        KC_4,     KC_5,      KC_6,     KC_7,      KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,             KC_PGUP,
+        CODE_DIR,  KC_GRV,              KC_1,     KC_2,     KC_3,        KC_4,     KC_5,      KC_6,     KC_7,      KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,             KC_PGUP,
         _______,  KC_TAB,              KC_Q,     KC_W,     KC_E,        KC_R,     KC_T,      KC_Y,     KC_U,      KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,             KC_PGDN,
         _______,  TD(TD_CAPS_CUSTOM),  KC_A,     KC_S,     KC_D,        KC_F,     KC_G,      KC_H,     KC_J,      KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,              KC_HOME,
         _______,  KC_LSFT,             KC_Z,     KC_X,     KC_C,        KC_V,     KC_B,      KC_N,     KC_M,      KC_COMM,  KC_DOT,   KC_SLSH,              TD(TD_RTSHFT_CAPS),  KC_UP,
