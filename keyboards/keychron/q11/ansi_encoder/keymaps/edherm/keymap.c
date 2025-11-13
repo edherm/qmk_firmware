2499f7f5b77ea19cfb271fa1e2012e3ffc5cb788
Ed Herman <eah286@cornell.edu>
2025-11-13 09:19:09 -0500

    Add Flow Shortcut

--- keyboards/keychron/q11/ansi_encoder/keymaps/edherm/keymap.c
+++ keyboards/keychron/q11/ansi_encoder/keymaps/edherm/keymap.c
@@ -25,6 +25,7 @@ enum layers{
 enum custom_keycodes {
     CTL_LEFT = SAFE_RANGE,
     CTL_RGHT,
+    FLOW_SHRTCT,
     TMR_NXT,
     TMR_STRT,
     MUT_ZM,
@@ -83,6 +84,19 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 unregister_code(KC_LCTL);
             }
             return false;
+        case FLOW_SHRTCT:
+            if (record->event.pressed) {
+                register_code(KC_LCTL);
+                register_code(KC_LALT);
+                register_code(KC_LSFT);
+                register_code(KC_LGUI);
+                tap_code(KC_F);
+                unregister_code(KC_LGUI);
+                unregister_code(KC_LSFT);
+                unregister_code(KC_LALT);
+                unregister_code(KC_LCTL);
+            }
+            return false;
         case TMR_NXT:
             if (record->event.pressed) {
                 register_code(KC_LCTL);
@@ -123,7 +137,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_91_ansi(
-        KC_MUTE,  KC_ESC,              KC_BRID,  KC_BRIU,  KC_MCTL,     KC_LPAD,  RM_VALD,   RM_VALU,  KC_MPRV,   KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  TMR_STRT,  KC_DEL,   KC_MUTE,
+        KC_MUTE,  KC_ESC,              KC_BRID,  KC_BRIU,  KC_MCTL,     KC_LPAD,  RM_VALD,   RM_VALU,  KC_MPRV,   KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  FLOW_SHRTCT,  KC_DEL,   KC_MUTE,
         _______,  KC_GRV,              KC_1,     KC_2,     KC_3,        KC_4,     KC_5,      KC_6,     KC_7,      KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,             KC_PGUP,
         _______,  KC_TAB,              KC_Q,     KC_W,     KC_E,        KC_R,     KC_T,      KC_Y,     KC_U,      KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,             KC_PGDN,
         _______,  TD(TD_CAPS_CUSTOM),  KC_A,     KC_S,     KC_D,        KC_F,     KC_G,      KC_H,     KC_J,      KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,              KC_HOME,
